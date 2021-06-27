a305d6fd49841f1714994501ef93418b52cf42fe
kulallador <ilyastalk@bk.ru>
2021-06-28 02:03:00 +0500

    add about, some changes

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
 from flask import Flask, render_template, request
-import numpy as np
 import json
 from PIL import Image
 import torch
 import io
 from base64 import b64encode
 import os
-import gc
-        
-# !pip install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt  
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, force_reload=True)
 model.eval()
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
-def hello_world():
+def get_index():
     return render_template("index.html")
+
+@app.route("/about")
+def get_about():
+    return render_template("about.html")
 
 def encode_img(img):
     data = io.BytesIO()
     img.save(data, "JPEG")
     res = b64encode(data.getvalue())
-    del data
-    gc.collect()
-    return  res
-    
+    data.close()
+
+    return  res 
 
 @app.route("/uploadimage", methods=["POST"])
 def upload_image():
@@ -37,21 +36,14 @@ def upload_image():
         img_bytes = file.read()
 
         img = Image.open(io.BytesIO(img_bytes))
-        img = img.convert("RGB")
-        uri = "data:image/jpeg;base64,"+encode_img(img).decode('utf-8')
 
         result = model(img, size=640)
         img_data = result.render()
         img_res = Image.fromarray(img_data[0])
         uri_res = "data:image/jpeg;base64,"+encode_img(img_res).decode('utf-8')
+        file.close()
 
-        del img
-        del img_res
-        del img_data
-        del img_bytes
-        gc.collect()
-
-        return json.dumps({"sourceImage": uri, "resultImage": uri_res})
+        return json.dumps({"resultImage": uri_res})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=port)
--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 var fileTypes = [
     'image/jpeg',
-    'image/pjpeg',
+    'image/jpg',
     'image/png'
 ]
 
@@ -18,60 +18,31 @@ function validFileType(file) {
 
 function getImageCard(path, name) {
     return `<div class="m-3">
-        <ul>
-            <li>
-            <img src="${path}" class="img-fluid rounded-3 shadow-lg border-dark imgCard">
-            </li>
-            <li>
-            <label class="text-center">${name}</label>
-            </li>
+        <img src="${path}" class="img-fluid rounded-3 shadow-lg border-dark imgCard">
     </div>`
 }
 
 formElem.onsubmit = async (e) => {
     e.preventDefault();
-
-    let response = await fetch('/uploadimage', {
-    method: 'POST',
-    body: new FormData(formElem)
-    });
-
-    let result = await response.json();
-    // img = document.createElement("img")
-
-    // img.src = result.image
-    // img.width = 400
-    // img.height = 300
-    // img.className = "img-fluid border rounded-3 shadow-lg mb-4 border border-dark"
-    sourceImage = getImageCard(result.sourceImage, "Source");
-    resultImage = getImageCard(result.resultImage, "Result")
-
-    prewievLabel.hidden = true;
-    imgDiv.innerHTML = sourceImage + resultImage;
-    // imgDiv.appendChild(img);
-    return false;
-};
-
-function sendImage() {
-    input = document.getElementById("myFile");
-    if (input.files[0] == null) {
+    if (formElem.myFile.files[0] == null){
         alert("Empty file");
-        return;
-    } 
-
-    file = input.files[0];
-    if (!validFileType(file)) {
-        alert("Bad format")
         return;
     }
 
+    if (!validFileType(formElem.myFile.files[0])) {
+        alert("Bad file");
+        return
+    }
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/uploadimage", true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            alert(xhr.responseText);
-        }
-    };
-    xhr.send(file);
-} 
+    let response = await fetch('/uploadimage', {
+        method: 'POST',
+        body: new FormData(formElem)
+    });
+
+    let result = await response.json();
+    resultImage = getImageCard(result.resultImage, "Result");
+
+    prewievLabel.hidden = true;
+    imgDiv.innerHTML = resultImage;
+    return false;
+};
